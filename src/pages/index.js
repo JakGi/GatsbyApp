b4ca65a1c57b777157137f47de6aab50c6d2c623
jakubjangala@gmail.com <jakubjangala@gmail.com>
2021-07-07 10:25:47 +0200

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import * as React from "react";
-
-
-
+import { Link } from "gatsby";
+import About from "./About";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>My Page</title>
+      <Link to="/src/pages/About.jsx"></Link>
     </main>
   );
 };
