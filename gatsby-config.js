cd619786edaa7175791f52aef331b3470ae76b3f
jakubjangala@gmail.com <jakubjangala@gmail.com>
2021-07-07 12:25:48 +0200

    logo added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "GatsbySite",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
-import { headerLink } from './header.module.css';
+import { headerLink, logo } from './header.module.css';
 
 const Header = () => {
     return (
         <div>
+            <StaticImage className={logo} src="../images/images.png"></StaticImage>
             <Link className={headerLink} to="/">Strona główna</Link>
             <Link className={headerLink} to="/blog">Blog</Link>
             <Link className={headerLink} to="/about">About</Link>
